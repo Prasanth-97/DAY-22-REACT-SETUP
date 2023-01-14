803875e99d4bb2551d237a5717f096179048591a
Prasanth-97 <prasanthdpj@gmail.com>
2023-01-14 12:25:08 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import "./App.css";
+import Message from "../src/constants/Message";
+
 
 export default function App() {
   return (
     <div className="App">
-     <Msg name="Ajith" pic="https://www.unigreet.com/wp-content/uploads/2021/07/WhatsApp-dp-images%EF%BB%BF-download%EF%BB%BF.jpg"/>
-     <Msg name="Vijay" pic="https://e1.pxfuel.com/desktop-wallpaper/718/825/desktop-wallpaper-22-3d-for-whatsapp-dp-cool-dp-thumbnail.jpg" />
-     <Msg name="Athulya" pic="https://funkylife.in/wp-content/uploads/2022/12/whatsapp-dp-funkylife-520-1.jpg"/>
+      {Message.length
+        ? Message.map((each) => <Msg name={each.name} pic={each.imageURL} />)
+        : ""}
     </div>
   );
 }
 
-function Msg({name,pic}){
-  return(
+function Msg({ name, pic }) {
+  return (
     <div>
-      <img className="person" src={pic} alt={name}/>
+      <img className="person" src={pic} alt={name} />
       <h2>Hi {name}❤️</h2>
-      </div>
-  )
+    </div>
+  );
 }
 
 // PROPS always comes as objects
---
+++ src/constants/Message.js
@@ -0,0 +1,19 @@
+ const MessageProps = [
+  {
+    name: "Ajith",
+    imageURL:
+      "https://www.unigreet.com/wp-content/uploads/2021/07/WhatsApp-dp-images%EF%BB%BF-download%EF%BB%BF.jpg",
+  },
+  {
+    name: "Athulya",
+    imageURL:
+      "https://funkylife.in/wp-content/uploads/2022/12/whatsapp-dp-funkylife-520-1.jpg",
+  },
+  {
+    name: "Vijay",
+    imageURL:
+      "https://e1.pxfuel.com/desktop-wallpaper/718/825/desktop-wallpaper-22-3d-for-whatsapp-dp-cool-dp-thumbnail.jpg",
+  },
+];
+
+export default MessageProps
